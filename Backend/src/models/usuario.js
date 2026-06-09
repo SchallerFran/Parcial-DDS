9c57808cef6e18516bc690241768fef0b1f70054
Francisco Schaller <franschaller91@gmail.com>
2026-06-09 17:20:10 -0300

    feat: setup inicial backend

---
+++ Backend/src/models/usuario.js
@@ -0,0 +1,10 @@
+import { DataTypes } from 'sequelize'
+import sequelize from '../config/db.js'
+
+export const Usuario = sequelize.define('Usuario', {
+  nombre:       { type: DataTypes.STRING, allowNull: false },
+  email:        { type: DataTypes.STRING, allowNull: false, unique: true },
+  passwordHash: { type: DataTypes.STRING, allowNull: false },
+  rol:          { type: DataTypes.ENUM('entrevistador', 'rrhh', 'admin'), allowNull: false },
+  activo:       { type: DataTypes.BOOLEAN, defaultValue: true }
+})
