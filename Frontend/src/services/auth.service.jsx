d1a10a1944b2356a90e8866b399c75cbe5bac944
fatarina1 <94588@sistemas.frc.utn.edu.ar>
2026-06-09 19:40:59 -0300

    Aut, log, reg, de usuario

---
+++ Frontend/src/services/auth.service.jsx
@@ -0,0 +1,42 @@
+import api from "../config/axios";
+
+const authService = {
+    // Iniciar sesion
+    async login(email, password) {
+        const {data} = await api.post("auth/login", {email, password});
+        
+        // Guardar token y usuario si el login es exitoso
+        localStorage.setItem("token", data.token); 
+        localStorage.setItem("usuario", JSON.stringify(data.usuario)); 
+        return data;
+    }   
+
+    // Registrar usuario
+    async register(nombre, email, password, rol = "entrevistador") {
+    const { data } = await api.post("/auth/register", { nombre, email, password, rol })
+    return data
+    },
+    
+    // Loguear usuario
+    logout() {
+    localStorage.removeItem("token")
+    localStorage.removeItem("usuario")
+    },
+
+    // Usuario guardado
+    getUsuarioGuardado() {    
+    const userLog = localStorage.getItem("usuario")    .
+    return userLog ? JSON.parse(raw) : null  
+    },
+
+    // Token y autenticacion
+    getToken() {    
+    return localStorage.getItem("token")
+    },
+
+    estaAutenticado() {
+    return !!localStorage.getItem("token")
+    }
+};
+
+export default authService
